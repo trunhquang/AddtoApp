98a29c4ab177c1cf01d488488c80eafb50cf807a
3321 <trunhquang@gmail.com>
2021-07-18 16:14:11 +0700

    Pre-warm the FlutterEngine. used FlutterEngineCache

--- app/src/main/java/com/concung/addflutter/MainActivity.kt
+++ app/src/main/java/com/concung/addflutter/MainActivity.kt
@@ -1,31 +1,51 @@
 package com.concung.addflutter
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
 
 
 class MainActivity : AppCompatActivity() {
+    var flutterEngine: FlutterEngine? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        findViewById<Button>(R.id.button).setOnClickListener{
-//            Log.d("adasdasd asdasdasd", "adasdasdasdasd")
-//            FlutterActivity.createDefaultIntent(this)
-//        }
+        // Instantiate a FlutterEngine.
+        flutterEngine = FlutterEngine(this)
+
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine?.dartExecutor?.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+
+        // Cache the FlutterEngine to be used by FlutterActivity.
+        FlutterEngineCache
+            .getInstance()
+            .put("my_engine_id", flutterEngine)
+
 
         findViewById<Button>(R.id.button).setOnClickListener {
+//            startActivity(
+//                FlutterActivity
+//                    .withNewEngine()
+////                    .initialRoute("/")
+//                    .build(this)
+//            )
             startActivity(
                 FlutterActivity
-                    .withNewEngine()
-//                    .initialRoute("/")
+                    .withCachedEngine("my_engine_id")
                     .build(this)
-            )
+            );
+
+
+
+
 
         }
 //        View flutterView = Flutter.createView();
